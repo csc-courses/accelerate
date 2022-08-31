fb251c8975488d3531d5fc2eab70603f50669138
burt <burt@cs.miami.edu>
2022-08-30 21:36:40 -0400

    ok

--- proj2/vector-mult.cu
+++ proj2/vector-mult.cu
@@ -16,7 +16,7 @@
 #endif
 
 // cuda kernel
-__global__ void sum_array(float * a, float *b, float * c) {
+__global__ void prod_array(float * a, float *b, float * c) {
 	int i = threadIdx.x + blockIdx.x * blockDim.x ;
 	a[i] = b[i] * c[i] ;
 	return ;
@@ -46,19 +46,19 @@ void printData(const char * s, float *ip, int n) {
 	printf("%s\t",s) ;
 	if (n<=f) {
 		for (i=0;i<n;i++) {
-			printf("%5.2f ", s, ip[i]) ;
+			printf("%5.2f\t", ip[i]) ;
 		}
 		printf("\n") ;
 		return ;
 	}
 	for (i=0;i<f;i++) {
-		printf("%5.2f ", s, ip[i]) ;
+		printf("%5.2f\t", ip[i]) ;
 	}
 	printf("\t...\t") ;
 	k = n - l ;
 	if (k<f) k = f ;
 	for (i=k;i<n;i++) {
-		printf("%5.2f ", s, ip[i]) ;
+		printf("%5.2f\t", ip[i]) ;
 	}
 	printf("\n") ;
 	return ;
@@ -68,7 +68,7 @@ float distance(float * a, float * b, float *c, int n) {
 	float f, dist = 0.0 ;
 	int i ;
 	for (i=0;i<n;i++) {
-		f = b[i] + c[i] - a[i] ;
+		f = b[i] * c[i] - a[i] ;
 		dist += f*f ;
 	}
 	return sqrt(dist) ;
@@ -98,14 +98,14 @@ int main(int argc, char * argv[]) {
 	cudaMemcpy(d_b, h_b, n_bytes, cudaMemcpyHostToDevice) ;
 	cudaMemcpy(d_c, h_c, n_bytes, cudaMemcpyHostToDevice) ;
 
-        sum_array <<<1,n>>> ( d_a, d_b, d_c ) ;
+        prod_array <<<1,n>>> ( d_a, d_b, d_c ) ;
 
 	cudaMemcpy(h_a, d_a, n_bytes, cudaMemcpyDeviceToHost) ;
 
-	printf("n =\t%d\n", n) ;
-	printData("b =", h_b,n) ;
-	printData("c =", h_c,n) ;
-	printData("sum =",h_a,n) ;
+	printf("n = %d\n", n) ;
+	printData("b =\n ", h_b,n) ;
+	printData("c =\n ", h_c,n) ;
+	printData("product =\n ",h_a,n) ;
 	printf("error = %f\n", distance(h_a,h_b,h_c,n) ) ;
 
 	cudaFree(d_a) ;
